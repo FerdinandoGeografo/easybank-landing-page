345dfde7cfd9a7cdf943b3ebb4eb91236753e836
FerdinandoGeografo <ferdinandogeografo@live.it>
2024-05-05 03:18:57 +0200

    general responsive design

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 // Make mobile navigation work:
 const btnNavEl = document.querySelector(".navigation__mobile-btn");
 const navEl = document.querySelector(".navigation");
+const heroEl = document.querySelector(".hero");
 
 btnNavEl.addEventListener("click", () => {
   navEl.classList.toggle("navigation__open");
+  heroEl.classList.toggle("hero--mask");
 });
